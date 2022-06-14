1fc9f23fb5cad3beef77a3dcd4f252b10e014f05
SirLynix <lynix680@gmail.com>
2022-06-14 08:38:37 +0200

    list-deps: Don't list the same lib dep twice

--- list-deps/main.lua
+++ list-deps/main.lua
@@ -8,38 +8,42 @@ function main()
     task.run("config", {require = false})
     os.cd(project.directory())
 
-	local requires, requires_extra = project.requires_str()
-	if not requires or #requires == 0 then
-		return
-	end
+    local requires, requires_extra = project.requires_str()
+    if not requires or #requires == 0 then
+        return
+    end
 
-	local usedpackages
+    local usedpackages
 
-	local targetname = option.get("target")
-	if targetname then
-		local target = project.target(targetname)
-		if not target then
-			os.raise("unknown target " .. targetname)
-		end
-		usedpackages = target:pkgs()
-	else
-		usedpackages = project.required_packages()
-	end
+    local targetname = option.get("target")
+    if targetname then
+        local target = project.target(targetname)
+        if not target then
+            os.raise("unknown target " .. targetname)
+        end
+        usedpackages = target:pkgs()
+    else
+        usedpackages = project.required_packages()
+    end
 
-	local projectdeps = {}
-	for _, instance in ipairs(package.load_packages(requires, {requires_extra = requires_extra})) do
-		if usedpackages[instance:name()] then
-			table.insert(projectdeps, {
-				name = instance:name(),
-				desc = instance:description(),
-				homepage = instance:get("homepage")
-			})
-		end
-	end
+    local listedpkgs = {}
+    local projectdeps = {}
+    for _, instance in ipairs(package.load_packages(requires, {requires_extra = requires_extra})) do
+        local name = instance:name()
+        if usedpackages[name] and not listedpkgs[name] then
+            table.insert(projectdeps, {
+                name = name,
+                desc = instance:description(),
+                homepage = instance:get("homepage")
+            })
 
-	table.sort(projectdeps, function (a, b) return a.name < b.name end)
+            listedpkgs[name] = true
+        end
+    end
 
-	for _, dep in pairs(projectdeps) do
-		print(string.format("- [%s](%s): %s", dep.name, dep.homepage, dep.desc))
-	end
+    table.sort(projectdeps, function (a, b) return a.name < b.name end)
+
+    for _, dep in pairs(projectdeps) do
+        print(string.format("- [%s](%s): %s", dep.name, dep.homepage, dep.desc))
+    end
 end
